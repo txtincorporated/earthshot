1c679a3d307e448c76bf95387e76b8c3775aeb8c
Nathan P. Keene <npk@txtincorporated.com>
2017-07-16 01:24:09 -0700

    Before block completed; starting it statements

--- test/flickrSvc.test.js
+++ test/flickrSvc.test.js
@@ -5,8 +5,17 @@ const chai = require( 'chai' ),
   app = require( '../lib/app' );
 
 describe('test 3d party API interface', () => {
-  
-}
-);
+  before(done => {
+    const drop = () => connection.db.dropDatabase( done );
+    if(connection.readyState === 1) drop( done );
+    else connection.on( 'open', drop ); 
+  }); 
+
+  const request = chai.request( app );
+
+  it('returns only photos posted since `max-upload-date`');
+
+
+});
 
 

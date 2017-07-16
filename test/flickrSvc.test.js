e6b7980020c560a0808ad1acbc47102e93ddcc95
Nathan P. Keene <npk@txtincorporated.com>
2017-07-16 00:41:33 -0700

    Start building flickr API svc test

---
+++ test/flickrSvc.test.js
@@ -0,0 +1,12 @@
+const chai = require( 'chai' ),
+  chaiHttp = require( 'chai-http' )
+  assert = chai.assert,
+  connection = require( '../lib/setup-mongoose' ),
+  app = require( '../lib/app' );
+
+describe('test 3d party API interface', () => {
+  
+}
+);
+
+

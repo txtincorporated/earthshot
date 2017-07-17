e922d89bfaf7a67c02d4da8e18ca54f26b9e3a74
Nathan P. Keene <npk@txtincorporated.com>
2017-07-16 17:58:43 -0700

    Separate db tests from API testing

--- test/flickrSvc.test.js
+++ test/flickrSvc.test.js
@@ -1,20 +1,44 @@
 const chai = require( 'chai' ),
   chaiHttp = require( 'chai-http' )
   assert = chai.assert,
-  connection = require( '../lib/setup-mongoose' ),
+  Flickr = require( 'flickrapi' ),
+  env = require( 'dotenv' ),
   app = require( '../lib/app' );
 
 describe('test 3d party API interface', () => {
   before(done => {
-    const drop = () => connection.db.dropDatabase( done );
-    if(connection.readyState === 1) drop( done );
-    else connection.on( 'open', drop ); 
+    const flickrOptions = {
+      api_key: 'env.TESTING_KEY',
+      api_secret: 'env.TESTING_SECRET'
+
+    };
+
+    let flickrRes;
+
+    Flickr.tokenOnly(flickrOptions, (error, flickr) => {
+      flickr.photos.search({
+        tags: 'architecture,landscape',
+        min_upload_date: '',
+        safe_search: '1',
+        content_type: '1',
+        media: 'photos',
+        has_geo: '1',
+        extras: 'date_upload, owner_name, geo, tags, url_s, url_z, url_o',
+        per_page: '250',
+        format: 'json',
+        nojsoncallback: '1'
+      }, (err, result) => {
+        if (err) { throw new Error(err); }
+        flickrRes = result.photos;
+        console.log( flickrRes );
+
+      });
+    });
   }); 
 
   const request = chai.request( app );
 
   it('returns only photos posted since `max-upload-date`');
-
 
 });
 
